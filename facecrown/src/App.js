f8eefbe84730cfed88bb50ef84a5666fbd2ad26e
GuiFxFarias <109701085+GuiFxFarias@users.noreply.github.com>
2023-04-04 05:51:33 -0300

    animation coin

--- facecrown/src/App.js
+++ facecrown/src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import { useState } from "react";
+import coin from "./assets/coin.png";
 
 function App() {
   const [played, setPlayed] = useState(false);
@@ -12,10 +13,12 @@ function App() {
     <>
       <div className="back">
         <h1 className="title">Cara ou coroa</h1>
-        <div className="coin"></div>
         <button className="playCoin" onClick={playCons}>
           Jogar Moeda
         </button>
+        <div className="coin">
+          <img src={coin} alt="Moeda" />
+        </div>
       </div>
     </>
   );
